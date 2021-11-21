31578889385c38214a94845dec0bba7f4e8aaa90
awesomenameman <soeren@collet.family>
2021-11-21 16:00:33 +0100

    Added test test_merge_arg_is_list
    This test will pass when func merge can raise an error if arg type
    is not a list.
    Refactored naming.

--- python/tests/test_merge/test_merge.py
+++ python/tests/test_merge/test_merge.py
@@ -4,10 +4,13 @@ Tests the merge function
 Functions:
     test_merge()
 """
-
 from merge.merge import merge
 
 
-def test_merge():
+def test_merge_arg_is_list():
+    """Test if exception raised with intervals arg not a list"""
+    merge("I will fail!")
+
+def test_merge_challenge():
     """Test if the challenge example is passed"""
     assert merge([[25,30],[2,19],[14,23],[4,8]]) == [[2,23],[25,30]]
