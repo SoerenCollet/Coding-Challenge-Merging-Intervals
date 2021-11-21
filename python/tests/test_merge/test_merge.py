9b6dcfba08e2d6dd0d19bf7fbb6bf3a5f79d2be7
awesomenameman <soeren@collet.family>
2021-11-21 21:05:23 +0100

    Added test for negative intervals

--- python/tests/test_merge/test_merge.py
+++ python/tests/test_merge/test_merge.py
@@ -11,6 +11,10 @@ def test_merge_arg_is_list():
     """Test if exception raised with intervals arg not a list"""
     merge("I will fail!")
 
+def test_merge_negative_intervals():
+    """Test if negative intervals can pass"""
+    assert merge([[-2,6],[-28,-8],[-9,-3]]) == [[-28,-3],[-2,6]]
+
 def test_merge_challenge():
     """Test if the challenge example is passed"""
     assert merge([[25,30],[2,19],[14,23],[4,8]]) == [[2,23],[25,30]]
