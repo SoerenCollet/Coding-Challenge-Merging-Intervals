0ebf6c2dab4ac26823a57d60ad759956e268fe50
awesomenameman <soeren@collet.family>
2021-11-25 19:54:19 +0100

    Checking raises instead of messages

--- python/tests/test_merge/test_merge.py
+++ python/tests/test_merge/test_merge.py
@@ -12,11 +12,13 @@ Functions:
     test_merge_challenge() -> None
 """
 from merge.merge import merge
+from pytest import raises
 
 
 def test_merge_arg_is_list():
     """Test if exception raised with intervals arg not a list"""
-    assert merge("I will fail!") == "TypeError: Intervals is not a list!"
+    with raises(TypeError):
+        assert merge("I will fail!")
 
 def test_merge_negative_intervals():
     """Test if negative intervals can pass"""
@@ -24,17 +26,18 @@ def test_merge_negative_intervals():
 
 def test_merge_2_values_per_interval():
     """Test if exception raised with interval values not equal 2"""
-    assert merge([[10], [23]]) == "ValueError: Please provide valid intervals!" \
-        "\nAn interval has exact 2 values."
+    with raises(ValueError):
+        merge([[10], [23]])
 
 def test_merge_start_interval_bigger_then_end_interval():
     """Test if exception raised with staring interval bigger then ending interval"""
-    assert merge([[100,50], [500,250]]) == "ValueError: Please provide valid intervals!" \
-        "\nThe starting interval should not be higher then the ending interval."
+    with raises(ValueError):
+        merge([[100,50], [500,250]])
 
 def test_merge_min_2_intervals():
     """Test if exception raised when not multiple intervals were provided"""
-    assert merge([[100,50]]) == "ValueError: Please provide at least 2 intervals to merge!"
+    with raises(ValueError):
+        merge([[100,50]])
 
 def test_merge_many_intervals(many_intervals):
     """Test if code can handle large number of intervals"""
